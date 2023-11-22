5cea7c1c1abce57df86073a1063e2e047f6d64fb
Kaushik Subramaniam Gnanaskandan <kgnanask@amazon.com>
2023-11-22 08:58:35 -0800

    fix: App needs to be deployed as purely compute without an adapter

--- bin/postbuild.sh
+++ bin/postbuild.sh
@@ -7,6 +7,7 @@ mkdir -p ./.amplify-hosting/compute
 cp -r ./dist/server ./.amplify-hosting/compute/default
 cp -r ./node_modules ./.amplify-hosting/compute/default/node_modules
 
-cp -r ./dist/client ./.amplify-hosting/static
+# Without the astro adapter, we can't do static asset separation
+# cp -r ./dist/client ./.amplify-hosting/static
 
 cp deploy-manifest.json ./.amplify-hosting/deploy-manifest.json
